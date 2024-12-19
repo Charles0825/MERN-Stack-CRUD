dfcfeb5a2b46d4e0837f63d3c2882933aafe5855
Charles Lim <charleslim0825@gmail.com>
2024-12-19 15:47:45 +0800

    first commit for mern stack crud operations

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import AddUser from "./adduser/addUser";
+import "./App.css";
+import User from "./getuser/User";
+import Update from "./updateuser/Update";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function App() {
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <User />,
+    },
+    {
+      path: "/add",
+      element: <AddUser />,
+    },
+    {
+      path: "/update/:id",
+      element: <Update />,
+    },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={route}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
